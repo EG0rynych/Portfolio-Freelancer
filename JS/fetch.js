6774635fb8979320edab21083d5787e341e1c1f9
EG0rynych <egorgorynych5720@gmail.com>
2025-08-04 14:18:11 +0300

    Исправление путей/фетч

--- JS/fetch.js
+++ JS/fetch.js
@@ -3,13 +3,13 @@ document.querySelectorAll('.nav-list__link[data-page]').forEach(link => {
       event.preventDefault();
       const page = link.dataset.page;
       try {
-         const res = await fetch(`HTML/${page}`);
+         const res = await fetch(`/Portfolio-Freelancer/HTML/${page}`);
          if (!res.ok) throw new Error(res.status);
          const html = await res.text();
          document.getElementById('main').innerHTML = html;
       } catch (e) {
-         console.error('Ошибка загрузки:',e);
+         console.error('Ошибка загрузки:', e);
          document.getElementById('main').textContent = 'Ошибка загрузки странички';
       }
-   })
-})
+   });
+});
